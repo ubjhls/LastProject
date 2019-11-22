320a89236190804949104c22228703b30a25cdcc
kimseonghoon <ubjhls@naver.com>
2019-11-22 14:56:12 +0900

    ex1

--- movies/views.py
+++ movies/views.py
@@ -3,6 +3,7 @@ from .models import Movie, Genre, Review
 from .forms import ReviewForm
 from django.views.decorators.http import require_POST
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseForbidden,HttpResponse,JsonResponse
 # Create your views here.
 def index(request):
     movies = Movie.objects.all()
@@ -45,9 +46,19 @@ def reviewDelete(request, movie_pk,review_pk):
 
 @login_required
 def like(request, movie_pk):
-    movie = get_object_or_404(Movie, pk=movie_pk)
-    if request.user in movie.like_users.all():
-        movie.like_users.remove(request.user)
+    if request.is_ajax():
+        movie = get_object_or_404(Movie, pk=movie_pk)
+        is_liked = True
+        if request.user in movie.like_users.all():
+            movie.like_users.remove(request.user)
+            is_liked = False
+        else:
+            movie.like_users.add(request.user)
+            is_liked = True
+        context = {
+            'is_liked' : is_liked,
+            'like_count' : movie.like_users.count()
+        }
+        return JsonResponse
     else:
-        movie.like_users.add(request.user)
-    return redirect('movies:detail', movie_pk)
+        return HttpResponseForbidden
