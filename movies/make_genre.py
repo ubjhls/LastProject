dcdd7e6c89131b3d8e74417a4b36db072021a711
sht3898 <sehyunteg@naver.com>
2019-11-25 17:30:53 +0900

    data 추가

--- movies/make_genre.py
+++ movies/make_genre.py
@@ -2,7 +2,10 @@ import requests
 import json
 import pprint
 
-url = 'https://api.themoviedb.org/3/genre/movie/list?api_key=1dfd52c8a24a0f38f40efe41c86be13b&language=ko-KR'
+# movie
+# url = 'https://api.themoviedb.org/3/genre/movie/list?api_key=1dfd52c8a24a0f38f40efe41c86be13b&language=ko-KR'
+# tv
+url = 'https://api.themoviedb.org/3/genre/tv/list?api_key=1dfd52c8a24a0f38f40efe41c86be13b&language=ko-KR'
 response = requests.get(url).json()
 pprint.pprint(response['genres'])
 genre_info = []
@@ -18,5 +21,5 @@ for genre in response['genres']:
     genre_info.append(new_genre)
 pprint.pprint(genre_info)
 
-with open('movies/fixtures/genre.json', 'w', encoding='utf-8') as f:
+with open('movies/fixtures/tv_genre.json', 'w', encoding='utf-8') as f:
     json.dump(genre_info, f, ensure_ascii=False, indent="\t")
---
+++ movies/make_movie.py
@@ -0,0 +1,56 @@
+import requests
+import json
+import pprint
+
+movie_list = []
+movie_info = []
+
+for page in range(1, 11):
+    # now_playing
+    # url = f'https://api.themoviedb.org/3/movie/now_playing?api_key=1dfd52c8a24a0f38f40efe41c86be13b&language=ko-KR&page={page}'
+    # popular
+    # url = f'https://api.themoviedb.org/3/movie/popular?api_key=1dfd52c8a24a0f38f40efe41c86be13b&language=ko-KR&page={page}'
+    # top_rated
+    url = f'https://api.themoviedb.org/3/movie/top_rated?api_key=1dfd52c8a24a0f38f40efe41c86be13b&language=ko-KR&page={page}'    
+    response = requests.get(url).json()
+    for movie in response["results"]:
+        movie_list.append(movie["id"])
+
+for movie in movie_list:
+    url = f"https://api.themoviedb.org/3/movie/{movie}?api_key=1dfd52c8a24a0f38f40efe41c86be13b&language=ko-KR"
+    response = requests.get(url).json()
+    video_url = f"https://api.themoviedb.org/3/movie/{movie}/videos?api_key=1dfd52c8a24a0f38f40efe41c86be13b&language=ko-KR"
+    video_response = requests.get(video_url).json()
+    if len(video_response["results"]) > 0:
+        video_link = f'https://www.youtube.com/watch?v={video_response["results"][0]["key"]}'
+    else:
+        video_link = 'https://www.youtube.com/'
+    if len(response['genres']) > 0:
+        genres = response['genres'][0]['id']
+    else:
+        genres = 18
+    if response['backdrop_path'] == "null":
+        back_image = 'https://image.flaticon.com/icons/svg/20/20773.svg'
+    else:
+        back_image = response['backdrop_path']
+    print(response['id'])
+    new_movie = {
+        'pk': response['id'],
+        'model': "movies.Movie",
+        'fields': {
+            'title': response['title'],
+            'movie_type': 'top_rated',
+            'poster_url': f"https://image.tmdb.org/t/p/w500{response['poster_path']}",
+            'description': response['overview'],
+            'genre_id': genres,
+            'video_link': video_link,
+            'popularity': response['popularity'],
+            'vote_average': response['vote_average'],
+            'back_image': back_image
+        }
+    }
+    movie_info.append(new_movie)
+
+pprint.pprint(movie_info)
+with open('movies/fixtures/top_rated.json', 'w', encoding='utf-8') as f:
+    json.dump(movie_info, f, ensure_ascii=False, indent="\t")
