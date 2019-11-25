c71da06af440f82ac492d41805f0150c1fd27702
sht3898 <sehyunteg@naver.com>
2019-11-25 09:18:50 +0900

    data

--- movies/make.py
+++
@@ -1,7 +0,0 @@
-import os
-import sys
-import urllib.request
-import requests
-
-response = requests.get('https://api.themoviedb.org/3/movie/11?api_key=1dfd52c8a24a0f38f40efe41c86be13b&language=ko-KR').json()
-print(response)
