9b3e40076921c698aa077c30f1bb497de45b2a94
kimseonghoon <ubjhls@naver.com>
2019-11-28 14:48:08 +0900

    final?

--- LastProject/settings/base.py
+++ LastProject/settings/base.py
@@ -152,6 +152,5 @@ AUTHENTICATION_BACKENDS = (
 SITE_ID = 1 # 'django.contrib.sites -> SITE_ID 부여
 LOGIN_REDIRECT_URL = '/movies/'
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
-
-MEDIA_ROOT = os.path.join(BASE_DIR, 'accounts/media')
--- movies/models.py
+++ movies/models.py
@@ -4,14 +4,14 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 # Create your models here.
 
 class Genre(models.Model):
-    name = models.CharField(max_length=30)
+    name = models.TextField()
 
 class People(models.Model):
-    name = models.CharField(max_length=100)
-    profile_path = models.CharField(max_length=140)
+    name = models.TextField()
+    profile_path = models.TextField()
 
 class Movie(models.Model):
-    title = models.CharField(max_length=40)
+    title = models.TextField()
     movie_type = models.TextField()
     poster_url = models.TextField()
     description =  models.TextField()
@@ -34,7 +34,7 @@ class Movie(models.Model):
     # sum_likes = models.IntegerField(blank=True)
 
 class Review(models.Model):
-    content = models.CharField(max_length=100, verbose_name='한줄평')
+    content = models.TextField(verbose_name='한줄평')
     score = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(10)], verbose_name='평점')
     movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
