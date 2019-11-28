b2404042b1f53a0970d2ad834834bb1ecc7960e9
sht3898 <sehyunteg@naver.com>
2019-11-28 09:47:41 +0900

    Settings.py 분리

--- LastProject/settings/base.py
+++ LastProject/settings/base.py
@@ -13,19 +13,22 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(
+    os.path.dirname(
+        os.path.dirname(os.path.abspath(__file__))))
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '8d#jyg@ym^^=51tq)_hhrx7*3i#=bx+)q4vg=^=oi+h_4%+j&r'
+from decouple import config
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
 
-ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = []
 
 
 # Application definition
